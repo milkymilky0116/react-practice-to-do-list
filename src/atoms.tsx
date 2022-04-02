8bf2b838dbcfce9a55caa8a71bf6002e238c06dc
MangoSteen0903 <sjlee990129@gmail.com>
2022-04-02 11:40:21 +0900

    Learning Set Selectors

--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,46 +1,18 @@
 import { atom, selector } from "recoil";
 
-let localId = "toDo";
-
-export enum Categories {
-  "TO_DO" = "TO_DO",
-  "Doing" = "Doing",
-  "Done" = "Done",
-}
-export interface IToDo {
-  text: string;
-  category: Categories;
-  id: number;
-}
-
-const localStorageEffect =
-  (key: string) =>
-  ({ setSelf, onSet }: any) => {
-    const savedValue = localStorage.getItem(localId);
-    if (savedValue != null) {
-      setSelf(JSON.parse(savedValue));
-    }
-    onSet((newValue: any, _: any, isReset: boolean) => {
-      isReset
-        ? localStorage.removeItem(localId)
-        : localStorage.setItem(localId, JSON.stringify(newValue));
-    });
-  };
-export const toDoState = atom<IToDo[]>({
-  key: "toDo",
-  default: [],
-  effects: [localStorageEffect(localId)],
+export const minuteState = atom({
+  key: "minutes",
+  default: 0,
 });
 
-export const categoryState = atom<Categories>({
-  key: "category",
-  default: Categories.TO_DO,
-});
-export const toDoSelector = selector({
-  key: "toDoSelector",
+export const hourSelector = selector<number>({
+  key: "hours",
   get: ({ get }) => {
-    const toDos = get(toDoState);
-    const category = get(categoryState);
-    return toDos.filter((toDo) => toDo.category === category);
+    const minutes = get(minuteState);
+    return minutes / 60;
+  },
+  set: ({ set }, newValue) => {
+    const minutes = +newValue * 60;
+    set(minuteState, minutes);
   },
 });
